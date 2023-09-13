18ddd3a698aca6d4648c90905146c78c3ebf5209
Adnan Wazwaz <adnanwazwaz1420@gmail.com>
2023-09-12 20:47:25 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
+const LOWERCASE_A_CODE = 97;
+const LOWERCASE_Z_CODE = 122;
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  /*
+  Formats the word by converting it to an array of lowercase
+  letters, and filtering out non-letters.
+  */
+  let processedWord = word.toLowerCase().split('').filter((char) => {
+    const charCode = char.charCodeAt(0);
+    return (charCode >= LOWERCASE_A_CODE && charCode <= LOWERCASE_Z_CODE);
+  });
+  let left = 0, right = processedWord.length - 1;
+  // Median index; note: value may be a decimal
+  const center = (processedWord.length - 1) / 2;
+  while (center > left && center < right) {
+    if (processedWord[left] !== processedWord[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  function isPalindrome(string)
+    string.toLowercase()
+    string.filterToLetters()
+
+    let left = 0;
+    let right = string.length - 1;
+    const center = ((string + 1) / 2) - 1 = (s - 1) / 2
+    while (left < center < right) {
+      if (string[left] !== string[right]) {
+        return false;
+      }
+      left++;
+      right--;
+    }
+    return true;
 */
 
 /*
